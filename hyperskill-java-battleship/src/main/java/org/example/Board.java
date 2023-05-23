ca4b8aab71fd45d0a60b59f069c10455231c44ca
Olga Fomenkova <olga.fomenkova@pprcard.ru>
2023-05-23 20:41:13 +0300

    Printing empty board

--- hyperskill-java-battleship/src/main/java/org/example/Board.java
+++ hyperskill-java-battleship/src/main/java/org/example/Board.java
@@ -2,8 +2,37 @@ package org.example;
 
 public class Board {
     private static final int BOARD_SIZE = 10;
+    private String[][] board = new String[BOARD_SIZE + 1][BOARD_SIZE + 1];
 
-    Board(int size) {
+    public Board() {
+        board[0][0] = " ";
+        // Заполняем цифры по оси X
+        for (int i = 1; i < BOARD_SIZE + 1; i++) {
+            board[0][i] = Integer.toString(i);
+        }
 
+        // Заполняем буквы по оси Y
+        for (char i = 'A'; i <= 'J'; i++) {
+            board[i - 'A' + 1][0] = Character.toString(i);
+        }
+
+        // Закрашиваем доску
+        for (int i = 1; i < BOARD_SIZE + 1; i++) {
+            for (int j = 1; j < BOARD_SIZE + 1; j++) {
+                board[i][j] = "~";
+            }
+
+        }
     }
+
+    public void printBoard() {
+        for (int i = 0; i < BOARD_SIZE + 1; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < BOARD_SIZE + 1; j++) {
+                System.out.print(" " + board[i][j]);
+            }
+        }
+    }
+
+
 }
