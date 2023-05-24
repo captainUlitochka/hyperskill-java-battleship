5725d8004a34ad2e133a7df6688c808c119f5163
Olga Fomenkova <olga.fomenkova@pprcard.ru>
2023-05-24 17:59:57 +0300

    Now I can draw new ships on game board

--- hyperskill-java-battleship/src/main/java/org/example/Board.java
+++ hyperskill-java-battleship/src/main/java/org/example/Board.java
@@ -1,16 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Board {
     private static final int BOARD_SIZE = 10;
+    private static final int SHIP_COUNT = 5;
     private final String[][] board = new String[BOARD_SIZE + 1][BOARD_SIZE + 1];
     private static final String FOG = "~";
     private static final String SHIP = "O";
     private static final String HIT = "X";
     private static final String MISS = "M";
+    Ship[] ships = new Ship[SHIP_COUNT];
 
 
     public Board() {
-        board[0][0] = " ";
+        board[0][0] = " "; // [по вертикали] [по горизонтали]
         // Заполняем цифры по оси X
         for (int i = 1; i < BOARD_SIZE + 1; i++) {
             board[0][i] = Integer.toString(i);
@@ -39,5 +43,36 @@ public class Board {
         }
     }
 
+    public void askForShip() {
+        byte shipIndex = 0;
+        for (ShipType shipType : ShipType.values()) {
+            addShipToBoard(shipType, shipIndex);
+            shipIndex++;
+        }
+    }
+
+    void addShipToBoard(ShipType type, byte shipIndex) {
+        Scanner scanner = new Scanner(System.in);
+        Ship ship;
+        // Запрос координат корабля
+        do {
+            System.out.printf("\nEnter the coordinates of the %s (%d cells):\n", type.getName(), type.getSize());
+            ship = new Ship(scanner.nextLine(), type);
+            System.out.println(ship.getErrorState());
+        } while (!ship.isValid());
+        ships[shipIndex] = ship;
+
+        // Рисование кораблей на карте
+        if (!ship.isVertical()) {
+            for (int i = ship.startPoint.getX(); i < ship.startPoint.getX() + ship.getLength(); i++) {
+                board[ship.startPoint.getYIndex()][i] = SHIP;
+            }
+        } else {
+            for (int i = ship.startPoint.getYIndex(); i < ship.startPoint.getYIndex() + ship.getLength(); i++) {
+                board[i][ship.startPoint.getX()] = SHIP;
+            }
+            printBoard();
+        }
+    }
 
 }
--- hyperskill-java-battleship/src/main/java/org/example/Ship.java
+++ hyperskill-java-battleship/src/main/java/org/example/Ship.java
@@ -20,7 +20,27 @@ public class Ship {
         return (byte) Math.abs(startPoint.getYIndex() - endPoint.getYIndex());
     }
 
-    public String validateLength() {
+    boolean isVertical() {
+        return getYLength() > 0;
+    }
+
+    byte getLength() {
+        if (isVertical()) {
+            System.out.println("Длина по Y: " + getYLength() + 1); //TODO: удалить отладочное
+            return (byte) (getYLength() + 1);
+        } else {
+            System.out.println("Длина по Х: " + getXLength() + 1); //TODO: удалить отладочное
+            return (byte) (getXLength() + 1);
+        }
+    }
+
+    public String getErrorState() {
+        isValid();
+        return errorState;
+    }
+
+    // Проверка, что корабль не вылезает за границы доски
+    String validateBorders() {
         if (startPoint.getX() < 1 || startPoint.getX() > 10 ||
                 endPoint.getX() < 1 || endPoint.getX() > 10 ||
                 startPoint.getYIndex() < 1 || startPoint.getYIndex() > 10 ||
@@ -28,7 +48,11 @@ public class Ship {
         ) {
             errorState = "Error! Wrong ship location! Try again:\n";
         }
+        return errorState;
+    }
 
+    // Проверка, что координаты корабля соответсвуют его дефолтной длине
+    String validateLength() {
         if (!((getXLength() == (type.getSize() - 1) && getYLength() == 0) ||
                 (getYLength() == (type.getSize() - 1) && getXLength() == 0))) {
             errorState = "Error! Wrong length of the " + type.getName() + "! Try again:\n";
@@ -37,6 +61,15 @@ public class Ship {
     }
 
     public boolean isValid() {
-        return validateLength().length() == 0;
+        return validateLength().length() == 0 && validateBorders().length() == 0;
+    }
+
+    boolean isCrossing() {
+        return false;
+    }
+
+    boolean isTooClose() {
+        return false;
+        // Check the field with coordinates (start row - 1) to (end row + 1) and (start column -1) to (end column +1) for occupied characters.
     }
 }
